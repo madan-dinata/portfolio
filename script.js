56432735626f4faaf95ada6697a4a4d0838d5189
madan-dinata <madanmr48@gmail.com>
2021-05-04 22:08:32 +0700

    initial commit

--- script.js
+++ script.js
@@ -24,3 +24,26 @@ window.addEventListener("click", (e) => {
     tutup();
   }
 });
+
+// dark mode
+if (localStorage.getItem("preferredTheme") == "dark") {
+  setDarkMode(true);
+}
+let dark = document.getElementById("darkBtn");
+
+function setDarkMode() {
+  let nav = document.getElementsByTagName("nav")[0];
+  let footer = document.getElementsByTagName("footer")[0];
+  let card = document.querySelectorAll(".card");
+  document.body.classList.toggle("darkmode");
+  nav.classList.toggle("navbar-light");
+  nav.classList.toggle("navbar-dark");
+  nav.classList.toggle("bg-white");
+  nav.classList.toggle("bg-dark");
+  footer.classList.toggle("text-light");
+  footer.classList.toggle("bg-dark");
+  footer.classList.toggle("border-top");
+  card.forEach((x) => x.classList.toggle("bg-dark"));
+}
+
+dark.addEventListener("click", setDarkMode);
